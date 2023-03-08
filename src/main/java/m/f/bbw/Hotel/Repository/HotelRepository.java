6e679f44bef53e9c39c4b4d0bca6757f33c80041
TAAFAMAK <marua.farag@swisscom.com>
2023-03-09 00:29:35 +0100

    repo to retrieve all besucher for a specific hotel

--- src/main/java/m/f/bbw/Hotel/Repository/HotelRepository.java
+++ src/main/java/m/f/bbw/Hotel/Repository/HotelRepository.java
@@ -1,4 +1,13 @@
 package m.f.bbw.Hotel.Repository;
 
-public class HotelRepository {
+import m.f.bbw.Hotel.Entities.Besucher;
+import m.f.bbw.Hotel.Entities.Hotel;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface HotelRepository extends CrudRepository<Hotel, Long> {
+    List<Besucher> findBesucherByHotel(Hotel hotel);
 }
